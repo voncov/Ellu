84e1e01c5e1744eee98edc429ae1e780ab2acfa8
Nullverity <testezeroane@gmail.com>
2025-11-21 16:57:36 +0300

    perf(system): (21-11-2025) Добавлены вызовы io_wait в инициализацию UART

--- src/includes/ports/COMX/UART.c
+++ src/includes/ports/COMX/UART.c
@@ -1,7 +1,7 @@
 /**
  * UART-модуль последовательного ввода/вывода в COM1-COM8 порты
  * 
- * Источники, взятые за пример:
+ * Источники с полезной информацией использовавшейся здесь:
  *      https://wiki.osdev.org/Serial_Ports
  */
 #include "UART.h"
@@ -19,19 +19,26 @@ int32_t init_serial(COMX_PORT comx_port)
     uint8_t test_char = 0xAE;
     // Включить прерывания у порта
     outb((uint16_t)comx_port + 1, (uint8_t)0x00);
+    io_wait();
     // Включить DLAB (Divisor Latch Access Bit || Бит доступа к Блокировке делителя)
     outb((uint16_t)comx_port + 3, (uint8_t)0x80);
     io_wait();
-    outb((uint16_t)comx_port + 0, (uint8_t)0x03); // Включить делитель на 3 (нижний байт)
-    outb((uint16_t)comx_port + 1, (uint8_t)0x00); //                        (высший байт)
+    outb((uint16_t)comx_port + 0, (uint8_t)0x03); // Включить делитель на 3 (младший байт)
+    io_wait();
+    outb((uint16_t)comx_port + 1, (uint8_t)0x00); //                        (старший байт)
+    io_wait();
     outb((uint16_t)comx_port + 3, (uint8_t)0x03); // 8 бит, без четности, один стоп-бит
+    io_wait();
     outb((uint16_t)comx_port + 2, (uint8_t)0xC7); // Включить FIFO, очистить их с 14-байтовым порогом
+    io_wait();
     outb((uint16_t)comx_port + 4, (uint8_t)0x0B); // IRQs включен, RTS/DSR установлен
+    io_wait();
     // Установить в режим обратной связи, чтобы тестировать последовательный чип
     outb((uint16_t)comx_port + 4, (uint8_t)0x1E);
     io_wait();
     // Тестируем последовательный чип (отправляем байт 0xAE и смотрим что вернёт последовательный порт)
     outb((uint16_t)comx_port + 0, (uint8_t)test_char);
+    io_wait();
     
     // Проверяем, не исправен ли последовательный порт
     uint8_t test_result = inb((uint16_t)comx_port + 0);
@@ -42,6 +49,7 @@ int32_t init_serial(COMX_PORT comx_port)
     // Если последовательный порт исправен, перевести его в нормальный режим работы
     // (без обратной связи с включенными IRQS и включенными битами OUT#1 и OUT#2)
     outb((uint16_t)comx_port + 4, (uint8_t)0x0F);
+    io_wait();
     current_port = comx_port;
     return 0;
 }
