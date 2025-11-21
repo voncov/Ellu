77cd2bab4dba9a745667362d6c7afe49c7325e65
Nullverity <testezeroane@gmail.com>
2025-11-21 19:12:08 +0300

    feat(system): (21-11-2025) Добавлена функция переназначения программируемого контроллера прерываний (ПКП или же PIC)
    Также изменён dbt/main.c, где наглядно демонстрируется переназначение PIC.

--- src/dbt/main.c
+++ src/dbt/main.c
@@ -8,10 +8,12 @@
 #include <ports/COMX/UART.h>
 
 void main(void) {
+    PIC_remap(0x20, 0x28);
     COMX_PORT port = COM1;
     init_serial(port);
     IO_MODE mode = SERIAL;
     set_io_mode(mode);
+    print("PIC remapped!");
 
     while (1);
 }
--- src/includes/cpu/procio.h
+++ src/includes/cpu/procio.h
@@ -1,13 +1,36 @@
 /**
  * Заголовочный файл модуля управления портами процессора Intel x86_x64
  * 
- * Источники, взятые за пример:
+ * Источники с полезной информацией использовавшейся здесь:
  *      https://wiki.osdev.org/Inline_Assembly/Examples
  *      https://wiki.osdev.org/Shutdown
+ *      https://wiki.osdev.org/8259_PIC
  */
 #pragma once
 
 #include "../stdint.h"
+
+#define PIC1 0x20               // Порт главного PIC
+#define PIC2 0xA0               // Порт ведомого PIC
+#define PIC1_CMD PIC1
+#define PIC1_DATA (PIC1 + 1)
+#define PIC2_CMD PIC2
+#define PIC2_DATA (PIC2 + 1)
+#define PIC_EOI 0x20            // Значение для завершения обработки прерывания (IRQ)
+
+#define ICW1_ICW4 0x01		    // Указывает на то, что ICW4 будет присутствовать
+#define ICW1_SINGLE	0x02		// Одиночный (каскадный) режим
+#define ICW1_INTERVAL4 0x04		// Интервал между вызовами 4 (8)
+#define ICW1_LEVEL 0x08		    // Режим срабатывания уровня (пограничный)
+#define ICW1_INIT 0x10          // Инициализация (обязательно)
+
+#define ICW4_8086 0x01		    // Режим 8086/88 (MCS-80/85)
+#define ICW4_AUTO 0x02		    // Автоматическое (обычное) завршение обработки прерывания 
+#define ICW4_BUF_SLAVE 0x08		// Буферизованный режим/ведомый
+#define ICW4_BUF_MASTER	0x0C	// Буферизованный режим/главный
+#define ICW4_SFNM 0x10		    // Специальный полностью вложенный (нет)
+
+#define CASCADE_IRQ 2
 
 /**
  * ### halt
@@ -42,7 +65,9 @@ static inline void interrupts_disable(void)
 /**
  * ### outb
  * 
- * Отправить 8-разрядное значение в место ввода/вывода.
+ * Отправить 8-разрядное значение в порт ввода/вывода
+ * @param port Порт ввода/вывода
+ * @param val Значение помещаемое в порт
  */
 static inline void outb(uint16_t port, uint8_t val)
 {
@@ -52,7 +77,9 @@ static inline void outb(uint16_t port, uint8_t val)
 /**
  * ### outw
  * 
- * Отправить 16-разрядное значение в место ввода/вывода.
+ * Отправить 16-разрядное значение в порт ввода/вывода
+ * @param port Порт ввода/вывода
+ * @param val Значение помещаемое в порт
  */
 static inline void outw(uint16_t port, uint16_t val)
 {
@@ -62,7 +89,9 @@ static inline void outw(uint16_t port, uint16_t val)
 /**
  * ### outl
  * 
- * Отправить 32-разрядное значение в место ввода/вывода.
+ * Отправить 32-разрядное значение в порт ввода/вывода
+ * @param port Порт ввода/вывода
+ * @param val Значение помещаемое в порт
  */
 static inline void outl(uint16_t port, uint32_t val)
 {
@@ -72,7 +101,9 @@ static inline void outl(uint16_t port, uint32_t val)
 /**
  * ### inb
  * 
- * Получить 8-разрядное значение из места ввода-вывода.
+ * Получить 8-разрядное значение из порта ввода/вывода.
+ * @param port Порт ввода/вывода
+ * @returns 8-разрядное значение из порта ввода/вывода
  */
 static inline uint8_t inb(uint16_t port)
 {
@@ -88,7 +119,9 @@ static inline uint8_t inb(uint16_t port)
 /**
  * ### inw
  * 
- * Получить 16-разрядное значение из места ввода-вывода.
+ * Получить 16-разрядное значение из порта ввода/вывода.
+ * @param port Порт ввода/вывода
+ * @returns 16-разрядное значение из порта ввода/вывода
  */
 static inline uint16_t inw(uint16_t port)
 {
@@ -104,7 +137,9 @@ static inline uint16_t inw(uint16_t port)
 /**
  * ### inl
  * 
- * Получить 32-разрядное значение из места ввода-вывода.
+ * Получить 32-разрядное значение из порта ввода/вывода.
+ * @param port Порт ввода/вывода
+ * @returns 32-разрядное значение из порта ввода/вывода
  */
 static inline uint32_t inl(uint16_t port)
 {
@@ -158,4 +193,62 @@ static inline void cpu_reset(void)
     __asm__ __volatile__("lidt %0; int3" :: "m" (null_idtr));
     // shutdown если ничего так и не произошло
     shutdown();
+}
+
+/**
+ * ### PIC_sendEOI
+ * 
+ * Отправка в PIC завершения обработки прерывания (IRQ)
+ * @param irq Номер прерывания
+ */
+static inline void PIC_sendEOI(uint8_t irq)
+{
+    if (irq >= 0x08)
+    {
+        outb((uint16_t)PIC2_CMD, (uint8_t)PIC_EOI);
+    }
+    outb((uint16_t)PIC1_CMD, (uint8_t)PIC_EOI);
+}
+
+/**
+ * ### PIC_remap
+ * 
+ * Переназначение программируемого контроллера прерываний
+ * @param offset1 Смещение вектора основного PIC
+ * @param offset2 Смещение вектора ведомого PIC
+ */
+static inline void PIC_remap(int32_t offset1, int32_t offset2)
+{
+	outb((uint16_t)PIC1_CMD, (uint8_t)(ICW1_INIT | ICW1_ICW4));     // запускает последовательность инициализации (в каскадном режиме)
+	io_wait();
+	outb((uint16_t)PIC2_CMD, (uint8_t)(ICW1_INIT | ICW1_ICW4));
+	io_wait();
+	outb((uint16_t)PIC1_DATA, (uint8_t)offset1);                   // ICW2: Смещение вектора основного PIC
+	io_wait();
+	outb((uint16_t)PIC2_DATA, (uint8_t)offset2);                   // ICW2: Смещение вектора ведомого PIC
+	io_wait();
+	outb((uint16_t)PIC1_DATA, (uint8_t)(1 << CASCADE_IRQ));        // ICW3: Сообщить главному PIC, что в IRQ2 есть подчиненный PIC.
+	io_wait();
+	outb((uint16_t)PIC2_DATA, (uint8_t)0x02);                      // ICW3: Сообщить подчиненному PIC его каскадный идентификатор (0000 0010).
+	io_wait();
+	
+	outb((uint16_t)PIC1_DATA, (uint8_t)ICW4_8086);                 // ICW4: попросите обеих PIC использовать режим 8086 (а не режим 8080)
+	io_wait();
+	outb((uint16_t)PIC2_DATA, (uint8_t)ICW4_8086);
+	io_wait();
+
+	// Снимите маску с обеих PIC.
+	outb((uint16_t)PIC1_DATA, (uint8_t)0x00);
+	outb((uint16_t)PIC2_DATA, (uint8_t)0x00);
+}
+
+/**
+ * ### PIC_disable
+ * 
+ * Отключение программируемого контроллера прерываний
+ */
+static inline void PIC_disable(void)
+{
+    outb((uint16_t)PIC1_DATA, (uint8_t)0xFF);
+    outb((uint16_t)PIC2_DATA, (uint8_t)0xFF);
 }
